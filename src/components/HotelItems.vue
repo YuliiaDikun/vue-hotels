16bdd54cc94c951b520f7680c6456ae990c297f9
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-30 11:42:13 +0300

    fix: read more btn

--- src/components/HotelItems.vue
+++ src/components/HotelItems.vue
@@ -1,30 +1,31 @@
 <template>
-  <li      
-    class="bg-white shadow rounded-xl"
-  >
-    <div class="w-full h-80 overflow-hidden rounded-t-xl">
-      <img
-        :src="hotel.propertyImage.image.url"
-        :alt="hotel.propertyImage.image.description"
-        class="object-cover w-full h-full"
-      />
-    </div>
-    <div class="p-3">
-      <h3 class="inline-block font-semibold border-b-2 border-purple-300">
-        {{ hotel.name }}
-      </h3>
-      <p class="mb-2">{{ hotel.mapMarker.label }}</p>
-      <router-link
-        :to="{ name: 'hotelDetails', params: { id: hotel.id } }"
-        class="inline-block px-3 py-1 rounded border-2 border-purple-300 hover:bg-purple-300 hover:text-white transition-colors"
-        >Learn more</router-link
-      >
+  <li class="bg-white shadow rounded-xl flex flex-col h-full overflow-hidden">
+    <div class="flex flex-col h-full">
+      <div class="w-full h-80 overflow-hidden rounded-t-xl">
+        <img
+          :src="hotel.propertyImage.image.url"
+          :alt="hotel.propertyImage.image.description"
+          class="object-cover w-full h-full"
+        />
+      </div>
+      <div class="px-3">
+        <h3 class="inline-block font-semibold border-b-2 border-purple-300">
+          {{ hotel.name }}
+        </h3>
+        <p class="mb-2">{{ hotel.mapMarker.label }}</p>
+      </div>
+      <div class="mt-auto p-3">
+        <router-link
+          :to="{ name: 'hotelDetails', params: { id: hotel.id } }"
+          class="inline-block mt-auto px-3 py-1 rounded border-2 border-purple-300 hover:bg-purple-300 hover:text-white transition-colors"
+          >Read more</router-link
+        >
+      </div>
     </div>
   </li>
 </template>
 
 <script setup>
-
 const { hotel } = defineProps({
   hotel: {
     required: true,
